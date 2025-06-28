5377180dd6af6c621b076312050d1a4e700db4db
pkumbhre <84072192+pkumbhre@users.noreply.github.com>
2025-06-28 21:04:34 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -5,6 +5,6 @@ def call(String credId, String imageName){
                     usernameVariable: "dockerHubUser"
                     )]){
                 sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-                sh "docker tag "${imageName}" ${env.dockerHubUser}/${imageName}"
+                sh "docker tag ${imageName} ${env.dockerHubUser}/${imageName}"
                 sh "docker push ${env.dockerHubUser}/${imageName}:latest"
 }
